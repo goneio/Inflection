373ef05b065104dd1f19c1741dc92aac206b7560
Matthew Baggett <matthew@baggett.me>
2017-05-25 10:46:10 +0100

    Working tests, fixed some bugs

---
+++ tests/MainTest.php
@@ -0,0 +1,78 @@
+<?php
+namespace Gone\Inflection\Tests;
+
+use Gone\Inflection\Inflect;
+
+class MainTest extends \PHPUnit\Framework\TestCase
+{
+    public function wordDataProvider()
+    {
+        return [
+            ['berry', 'berries'],
+            ['activity', 'activities'],
+            ['daisy', 'daisies'],
+            ['church', 'churches'],
+            ['bus', 'buses'],
+            ['business', 'businesses'],
+            ['fox', 'foxes'],
+            ['stomach', 'stomachs'],
+            ['epoch', 'epochs'],
+            ['knife', 'knives'],
+            ['half', 'halves'],
+            ['scarf', 'scarves'],
+            ['chief', 'chiefs'],
+            ['spoof', 'spoofs'],
+            ['solo', 'solos'],
+            ['zero', 'zeroes'],
+            ['avocado', 'avocados'],
+            ['studio', 'studios'],
+            ['zoo', 'zoos'],
+            // Words of Latin origin, as per https://en.oxforddictionaries.com/spelling/plurals-of-nouns
+            ['alga', 'algae'],
+            ['alumnus', 'alumni'],
+            ['larva', 'larvae'],
+            // Words of Greek origin
+            ['crisis', 'crises'],
+            ['analysis', 'analyses'],
+            ['neurosis', 'neuroses'],
+            // Words of French origin
+            ['bureau', 'bureaus'],
+            ['chateau', 'chateaus'],
+            ['gateau', 'gateaus'],
+            ['trousseau', 'trousseaus'],
+            // Words of Italian origin
+            ['espresso', 'espressos'],
+            ['pizza', 'pizzas'],
+            ['risotto', 'risottos'],
+            ['fresco', 'frescos'],
+            // Uncountable
+            ['money', 'money'],
+            ['sheep', 'sheep']
+        ];
+    }
+
+    /**
+     * @dataProvider wordDataProvider
+     */
+    public function testSingularise($singular, $plural)
+    {
+        $this->assertEquals($singular, Inflect::singularize($plural));
+    }
+
+    /**
+     * @dataProvider wordDataProvider
+     */
+    public function testPluralise($singular, $plural)
+    {
+        $this->assertEquals($plural, Inflect::pluralize($singular));
+    }
+
+    /**
+     * @dataProvider wordDataProvider
+     */
+    public function testPluraliseIf($singular, $plural)
+    {
+        $this->assertEquals("2 {$plural}", Inflect::pluralizeIf(2, $singular));
+        $this->assertEquals("1 {$singular}", Inflect::pluralizeIf(1, $singular));
+    }
+}
